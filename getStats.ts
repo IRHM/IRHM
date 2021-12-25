cd18b6b1ad80237e06bb5ddc2c6c3999003b1cbd
IRHM <37304121+IRHM@users.noreply.github.com>
2021-12-25 02:37:20 +0000

    Show bullet and rapid stats and combined wins/losses/draws

--- getStats.ts
+++ getStats.ts
@@ -1,46 +1,78 @@
-import * as fs from 'fs';
-import axios from './node_modules/axios/index.js';
+import * as fs from "fs";
+import axios from "./node_modules/axios/index.js";
 
 interface stat {
-  person: string,
-  chess_blitz: chess_blitz
+  person: string;
+  data: {
+    chess_bullet: chess_bullet;
+    chess_blitz: chess_blitz;
+    chess_rapid: chess_rapid;
+  };
+}
+
+interface chess_bullet {
+  last: last;
+  best: best;
+  record: record;
 }
 
 interface chess_blitz {
-  last: last,
-  best: best,
-  record: record
+  last: last;
+  best: best;
+  record: record;
+}
+
+interface chess_rapid {
+  last: last;
+  best: best;
+  record: record;
 }
 
 interface last {
-  rating: number
+  rating: number;
 }
 
 interface best {
-  rating: number,
-  game: string
+  rating: number;
+  game: string;
 }
 
 interface record {
-  win: number,
-  loss: number,
-  draw: number
+  win: number;
+  loss: number;
+  draw: number;
 }
 
 class GetStats {
   async requestStats() {
-    const statsMe = await axios.get(process.env.URL_STAT_ME);
-    const statsMr = await axios.get(process.env.URL_STAT_MR);
-    const statsKingo = await axios.get(process.env.URL_STAT_KINGO);
-    const statsDan = await axios.get(process.env.URL_STAT_DAN);
-    const statsMar = await axios.get(process.env.URL_STAT_MAR);
+    // if (
+    //   process.env.URL_STAT_ME == undefined ||
+    //   process.env.URL_STAT_MR == undefined ||
+    //   process.env.URL_STAT_KINGO == undefined ||
+    //   process.env.URL_STAT_DAN == undefined ||
+    //   process.env.URL_STAT_MAR == undefined
+    // ) {
+    //   throw new Error("Some or all of the URL_STAT_... env vars are missing.");
+    // }
+
+    // const statsMe = await axios.get(process.env.URL_STAT_ME);
+    // const statsMr = await axios.get(process.env.URL_STAT_MR);
+    // const statsKingo = await axios.get(process.env.URL_STAT_KINGO);
+    // const statsDan = await axios.get(process.env.URL_STAT_DAN);
+    // const statsMar = await axios.get(process.env.URL_STAT_MAR);
+
+    const statsMe = await axios.get("https://api.chess.com/pub/player/sbondo1234/stats");
+    const statsMr = await axios.get("https://api.chess.com/pub/player/mrunknownman/stats");
+    const statsKingo = await axios.get("https://api.chess.com/pub/player/kingo2157/stats");
+    const statsDan = await axios.get("https://api.chess.com/pub/player/dan_richman/stats");
+    const statsMar = await axios.get("https://api.chess.com/pub/player/mizsoonhome/stats");
 
     var t: stat[] = [
-      { person: "Me", chess_blitz: statsMe.data.chess_blitz },
-      { person: "Mr 2 Gut", chess_blitz: statsMr.data.chess_blitz },
-      { person: "Kingo", chess_blitz: statsKingo.data.chess_blitz },
-      { person: "Dan", chess_blitz: statsDan.data.chess_blitz },
-      { person: "Marius", chess_blitz: statsMar.data.chess_blitz }
+      { person: "Me", data: statsMe.data },
+      { person: "Mr 2 Gut", data: statsMr.data },
+      { person: "Kingo", data: statsKingo.data },
+      { person: "Dan", data: statsDan.data },
+      { person: "Marius", data: statsMar.data }
     ];
 
     return t;
@@ -50,7 +82,7 @@ class GetStats {
 let getStats = new GetStats();
 
 // Write README
-getStats.requestStats().then(stats => {
+getStats.requestStats().then((stats) => {
   let toWrite = `
 ${process.env.BASE_README}
 
@@ -60,35 +92,47 @@ ${process.env.BASE_README}
 <tr>
   <td rowspan="1"></td>
   <th colspan="3" scope="colgroup">Game</th>
-  <th colspan="1" scope="colgroup">Latest</th>
-  <th colspan="1" scope="colgroup">Best</th>
+  <th colspan="3" scope="colgroup">Latest Rating</th>
+  <th colspan="3" scope="colgroup">Best Rating</th>
 </tr>
 <tr>
   <th>Person</th>
   <th>Wins</th>
   <th>Losses</th>
   <th>Draws</th>
-  <th>Rating</th>
-  <th>Rating</th>
+  <th>Bullet</th>
+  <th>Blitz</th>
+  <th>Rapid</th>
+  <th>Bullet</th>
+  <th>Blitz</th>
+  <th>Rapid</th>
 </tr>
   `;
 
-  stats.forEach(function(s) {
+  stats.forEach(function (s) {
+    let wins = s.data.chess_bullet.record.win + s.data.chess_blitz.record.win + s.data.chess_rapid.record.win;
+    let losses = s.data.chess_bullet.record.loss + s.data.chess_blitz.record.loss + s.data.chess_rapid.record.loss;
+    let draws = s.data.chess_bullet.record.draw + s.data.chess_blitz.record.draw + s.data.chess_rapid.record.draw;
+
     toWrite = toWrite.concat(`
 <tr>
   <td>${s.person}</td>
-  <td>${s.chess_blitz.record.win}</td>
-  <td>${s.chess_blitz.record.loss}</td>
-  <td>${s.chess_blitz.record.draw}</td>
-  <td>${s.chess_blitz.last.rating}</td>
-  <td>${s.chess_blitz.best.rating}</td>
+  <td>${wins}</td>
+  <td>${losses}</td>
+  <td>${draws}</td>
+  <td>${s.data.chess_bullet.last.rating}</td>
+  <td>${s.data.chess_blitz.last.rating}</td>
+  <td>${s.data.chess_rapid.last.rating}</td>
+  <td>${s.data.chess_bullet.best.rating}</td>
+  <td>${s.data.chess_blitz.best.rating}</td>
+  <td>${s.data.chess_rapid.best.rating}</td>
 </tr>
     \n`);
   });
 
   toWrite = toWrite.concat(`</table>`);
-  
-  fs.writeFile('README.md', toWrite, (err) => {
+
+  fs.writeFile("README.md", toWrite, (err) => {
     if (err) console.log("Couldn't write to README");
     else console.log("Wrote to README");
   });
